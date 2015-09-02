692803bc54d1e498c0b033490d9324a0617761b6
Francesco Biscani <bluescarni@gmail.com>
2015-09-02 14:48:13 +0200

    A few more initial cleanups.

--- src/gdual.hpp
+++ src/gdual.hpp
@@ -1,11 +1,14 @@
 #ifndef AUDI_GDUAL_HPP
 #define AUDI_GDUAL_HPP
 
-#include <stdexcept>
-#include <string>
+#include <algorithm>
+#include <initializer_list>
 #include <iostream>
 #include <piranha/polynomial.hpp>
-#include <algorithm> 
+#include <stdexcept>
+#include <string>
+#include <type_traits> // For std::enable_if, std::is_same, etc.
+#include <utility>
 
 #include "functions.hpp"
 
@@ -18,20 +21,28 @@ class gdual
 	using cf_type = double;
 	using p_type = piranha::polynomial<cf_type,piranha::k_monomial>;
 
-	// We disable the overloads of the +,-,*,/ operators if none of the operands is a gdual
+	// We enable the overloads of the +,-,*,/ operators only in the following cases:
+	// - at least one operand is a dual,
+	// - the other operand, if not dual, must be double or int.
 	template <typename T, typename U>
-	using gdual_if_enabled = typename std::enable_if<std::is_same<T,gdual>::value || std::is_same<U,gdual>::value,gdual>::type;
+	using gdual_if_enabled = typename std::enable_if<
+		(std::is_same<T,gdual>::value && std::is_same<U,gdual>::value) ||
+		(std::is_same<T,gdual>::value && std::is_same<U,double>::value) ||
+		(std::is_same<T,gdual>::value && std::is_same<U,int>::value) ||
+		(std::is_same<U,gdual>::value && std::is_same<T,double>::value) ||
+		(std::is_same<U,gdual>::value && std::is_same<T,int>::value),
+	gdual>::type;
 
 public:
 	explicit gdual(const std::string &str, int order):m_p(str),m_order(order) {
 		if (order < 1) {
-			throw std::invalid_argument("polynomial truncation order must be > 1");
+			throw std::invalid_argument("polynomial truncation order must be >= 1");
 		}
 	}
 
 	explicit gdual(double x, int order):m_p(x),m_order(order) {
 		if (order < 1) {
-			throw std::invalid_argument("polynomial truncation order must be > 1");
+			throw std::invalid_argument("polynomial truncation order must be >= 1");
 		}
 	}
 
@@ -43,22 +54,12 @@ public:
 	template <typename T>
 	cf_type find_cf(const T &c) const
 	{
-		if (!std::all_of(c.begin(), c.end(), [&](int i){return i<=get_order();}) ||
-		   !std::all_of(c.begin(), c.end(), [](int i){return i>=0;}))
-		{
-			throw std::invalid_argument("invalid order(s) requested for derivative ");
-		}
 		return m_p.find_cf(c);
 	}
 
 	template <typename T>
 	cf_type find_cf(std::initializer_list<T> l) const
 	{
-		if (!std::all_of(l.begin(), l.end(), [&](T i){return i<=get_order();}) || 
-		    !std::all_of(l.begin(), l.end(), [](T i){return i>=0;}))
-		{
-			throw std::invalid_argument("invalid order(s) requested for derivative ");
-		}
 		return m_p.find_cf(l);
 	}
 
@@ -71,6 +72,11 @@ public:
 	friend bool operator==(const gdual  &d1, const gdual &d2) 
 	{
 		return (d1.m_p == d2.m_p) && (d1.m_order == d2.m_order);
+	}
+
+	friend bool operator!=(const gdual  &d1, const gdual &d2)
+	{
+		return !(d1 == d2);
 	}
 
 	template <typename T, typename U>
@@ -87,7 +93,8 @@ public:
 
 private:
 	// Basic overloads for the addition
-	static gdual add(const gdual  &d1, const gdual  &d2) {
+	static gdual add(const gdual  &d1, const gdual  &d2)
+	{
 		if (d1.get_order() != d2.get_order()) {
 			throw std::invalid_argument("different truncation limit");
 		}
@@ -97,19 +104,22 @@ private:
 	}
 
 	template <typename T>
-	static gdual add(const T &d1, const gdual  &d2) {
+	static gdual add(const T &d1, const gdual  &d2)
+	{
 		gdual retval(d2);
 		retval.m_p += d1;
 		return retval;
 	}
 
 	template <typename T>
-	static gdual add(const gdual  &d1, const T &d2) {
+	static gdual add(const gdual  &d1, const T &d2)
+	{
 		return add(d2, d1);
 	}
 
 	// Basic overloads for the subtraction
-	static gdual sub(const gdual  &d1, const gdual  &d2) {
+	static gdual sub(const gdual  &d1, const gdual  &d2)
+	{
 		if (d1.get_order() != d2.get_order()) {
 			throw std::invalid_argument("different truncation limit");
 		}
@@ -119,14 +129,17 @@ private:
 	}
 
 	template <typename T>
-	static gdual sub(const T &d1, const gdual  &d2) {
+	static gdual sub(const T &d1, const gdual  &d2)
+	{
 		gdual retval(d2);
-		retval.m_p = d1 - retval.m_p;
+		retval.m_p -= d1;
+		retval.m_p.negate();
 		return retval;
 	}
 
 	template <typename T>
-	static gdual sub(const gdual  &d1, const T &d2) {
+	static gdual sub(const gdual  &d1, const T &d2)
+	{
 		gdual retval(d1);
 		retval.m_p -= d2;
 		return retval;
@@ -135,5 +148,7 @@ private:
 	p_type		m_p;
 	const int	m_order;
 };
+
 } // end of namespace audi 
+
 #endif
--- tests/gdual.cpp
+++ tests/gdual.cpp
@@ -1,13 +1,15 @@
-#define BOOST_TEST_MODULE gdual_test
+#include "../src/gdual.hpp"
+
+#define BOOST_TEST_MODULE audi_gdual_test
 #include <boost/test/unit_test.hpp>
 
-#include "../src/audi.hpp"
+#include <stdexcept>
+#include <vector>
 
 using namespace audi;
 
 BOOST_AUTO_TEST_CASE(add)
 {
-	piranha::environment env;
 	gdual p1(1, 4);
 	gdual p2("x", 4);
 	BOOST_CHECK(p1 + p2 == p2 + p1);
@@ -17,7 +19,6 @@ BOOST_AUTO_TEST_CASE(add)
 
 BOOST_AUTO_TEST_CASE(sub)
 {
-	piranha::environment env;
 	gdual p1(1, 4);
 	gdual p2("x", 4);
 	BOOST_CHECK(p1 - p1 == gdual(0,4));
@@ -31,10 +32,9 @@ BOOST_AUTO_TEST_CASE(sub)
 
 BOOST_AUTO_TEST_CASE(find_cf)
 {
-	piranha::environment env;
-	BOOST_CHECK_THROW(gdual("x",4).find_cf({5}),std::invalid_argument);
+	BOOST_CHECK_EQUAL(gdual("x",4).find_cf({5}),0.);
 	BOOST_CHECK_THROW(gdual(1,4).find_cf({5}),std::invalid_argument);
-	BOOST_CHECK_THROW(gdual("x",4).find_cf(std::vector<int>{5}),std::invalid_argument);
+	BOOST_CHECK_EQUAL(gdual("x",4).find_cf(std::vector<int>{5}),0.);
 	BOOST_CHECK_THROW(gdual(1,4).find_cf(std::vector<int>{5}),std::invalid_argument);
 
 	BOOST_CHECK_EQUAL((gdual(3,4) + gdual("x",4)).find_cf({0}),3);
