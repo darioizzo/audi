361da0dcb296d77ee8ea810a4d84f5e965e50cb5
Dario Izzo <dario.izzo@gmail.com>
2015-09-04 14:18:09 +0200

    test file added

---
+++ tests/functions.cpp
@@ -0,0 +1,21 @@
+#include "../src/gdual.hpp"
+#include "../src/functions.hpp"
+
+#define BOOST_TEST_MODULE audi_functions_test
+#include <boost/test/unit_test.hpp>
+
+#include <stdexcept>
+#include <vector>
+
+using namespace audi;
+
+BOOST_AUTO_TEST_CASE(power)
+{
+	gdual x("dx",3);
+	gdual y("dy",3);
+	x += 1;
+	y += 1;
+	auto p1 = x*x*y + x*y*x*x*x - 3*y*y*y*y*x*y*x;
+	BOOST_CHECK_EQUAL(pow(p1, 4), p1*p1*p1*p1);
+}
+
