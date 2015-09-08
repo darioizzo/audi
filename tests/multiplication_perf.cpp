37e1db8a0d201a6ee67ab51a6ed7a1e7a21c7899
Dario Izzo <dario.izzo@gmail.com>
2015-09-08 15:11:58 +0200

    multiplication now tests only one multiplication

--- tests/multiplication_perf.cpp
+++ tests/multiplication_perf.cpp
@@ -21,11 +21,12 @@ void scalable_mul(int m, int n)
     for (int i = 0u; i < n; ++i) {p1 += variables[i];} // 1 + x1 + x2 + ...
     for (int i = 0u; i < n; ++i) {p2 -= variables[i];} // 1 - x1 - x2 + ...
     {
-        boost::timer::auto_cpu_timer t; // We only time the time cost of the following operation
         auto result = p1 * p2;
-        for (auto i = 1; i < m; ++i) {
+        for (auto i = 1; i < m-1; ++i) {
             result*=result;
         }
+    boost::timer::auto_cpu_timer t; // We only time the time cost of the following operation
+    result*=result;
     }
 }
 
@@ -35,8 +36,8 @@ BOOST_AUTO_TEST_CASE(multiplication_performance)
         piranha::settings::set_n_threads(boost::lexical_cast<unsigned>(boost::unit_test::framework::master_test_suite().argv[1u]));
     }
     std::cout << "Testing multiplication of (1 + x1 + .. + xn)^m * (1 - x1 - .. - xn)^m: " << std::endl;
-    for (auto m = 5; m < 10; ++m) {
-        for (auto n = 5; n < 12; ++n) {
+    for (auto m = 5; m < 11; ++m) {
+        for (auto n = 5; n < 11; ++n) {
             scalable_mul(m,n);
         }
     }
