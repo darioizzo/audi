9004f2aee34c52711e853997ae84dd1ab9576a27
Dario Izzo <dario.izzo@gmail.com>
2016-02-05 09:49:13 +0100

    cos, exp and pow added

--- pyaudi/core.cpp
+++ pyaudi/core.cpp
@@ -49,8 +49,24 @@ PYBIND11_PLUGIN(_core) {
 		.def(int() / py::self)
 	;
 
+	m.def("exp",[](const gdual &d) {return exp(d);},"Exponential (gdual).");
+	m.def("exp",[](double x) {return std::exp(x);},"Exponential (double).");
+
+	m.def("log",[](const gdual &d) {return log(d);},"Natural logarithm (gdual).");
+	m.def("log",[](double x) {return std::log(x);},"Natural logarithm (double).");
+
+	m.def("pow",[](double base, const gdual &d) {return pow(base, d);},"Exponentiation (double, gdual).");
+	m.def("pow",[](const gdual & base, double d) {return pow(base, d);},"Exponentiation (gdual, double).");
+	m.def("pow",[](const gdual & base, int d) {return pow(base, d);},"Exponentiation (gdual, int).");
+	m.def("pow",[](const gdual & base, const gdual &d) {return pow(base, d);},"Exponentiation (gdual, gdual).");
+	m.def("pow",[](double base, double x) {return std::pow(base, x);},"Natural logarithm (double, double).");
+
 	m.def("sin",[](const gdual &d) {return sin(d);},"Sine (gdual).");
 	m.def("sin",[](double x) {return std::sin(x);},"Sine (double).");
+	// m.def("sin",py::vectorize([](double x) {return std::sin(x);}),"Sine (vectorized double).");
+
+	m.def("cos",[](const gdual &d) {return cos(d);},"Cosine (gdual).");
+	m.def("cos",[](double x) {return std::cos(x);},"Cosine (double).");
 
 	return m.ptr();
 }
