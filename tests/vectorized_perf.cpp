80e17405e3eca61cf6bb587db4567aa6d2ea5225
Dario Izzo <dario.izzo@gmail.com>
2016-09-25 22:51:39 +0200

    perf test cleaned and output improved

--- tests/vectorized_perf.cpp
+++ tests/vectorized_perf.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <piranha/polynomial.hpp>
 #include <piranha/type_traits.hpp>
 #include <vector>
@@ -21,45 +22,43 @@ std::vector<double> random_vector_double(unsigned int N, double lb, double ub)
     return retval;
 }
 
-using namespace piranha;
-
-int main() {
-    unsigned int N = 10000u;
+void measure_speedup(int points, unsigned int order)
+{
     auto lb = -0.5;
     auto ub = 0.5;
     // We generate random vector coefficients
-    auto xcoeff = random_vector_double(N, lb, ub);
-    auto ycoeff = random_vector_double(N, lb, ub);
-    auto zcoeff = random_vector_double(N, lb, ub);
-    auto coeff = random_vector_double(N, lb, ub);
+    auto xcoeff = random_vector_double(points, lb, ub);
+    auto ycoeff = random_vector_double(points, lb, ub);
+    auto zcoeff = random_vector_double(points, lb, ub);
+    auto coeff = random_vector_double(points, lb, ub);
     auto coeff_v = audi::gdual_v(coeff);
-    const auto order = 2u;
 
-    {
-        boost::timer::auto_cpu_timer t;
-        audi::gdual_v x{xcoeff, "x",order}, y{ycoeff, "y",order}, z{zcoeff, "z",order}, w{zcoeff, "w",order}, q{zcoeff, "q",order}, r{zcoeff, "r",order}, s{zcoeff, "s",order} ;
-        auto foo = coeff_v + x + y + z - w + q + r + s;
-        for (int i = 1; i < 25; ++i) {
-            foo *= coeff_v + x + y + z - w + q + r + s;
+    boost::timer::cpu_timer t1;
+    audi::gdual_v x{xcoeff, "x",order}, y{ycoeff, "y",order}, z{zcoeff, "z",order}, w{zcoeff, "w",order}, q{zcoeff, "q",order}, r{zcoeff, "r",order}, s{zcoeff, "s",order} ;
+    auto foo = coeff_v + x + y + z - w + q + r + s;
+    for (int i = 1; i < 20; ++i) {
+        foo *= coeff_v + x + y + z - w + q + r + s;
+    }
+    foo *= foo;
+    auto wall1 = t1.elapsed().wall;
+
+    boost::timer::cpu_timer t2;
+    for (auto i = 0u; i < points; ++i) {
+        audi::gdual_d x{xcoeff[i], "x",order}, y{ycoeff[i], "y",order}, z{zcoeff[i], "z",order}, w{zcoeff[i], "w",order}, q{zcoeff[i], "q",order}, r{zcoeff[i], "r",order}, s{zcoeff[i], "s",order} ;
+        auto foo = coeff[i] + x + y + z - w + q + r + s;
+        for (int j = 1; j < 20; ++j) {
+            foo *= coeff[i] + x + y + z - w + q + r + s;
         }
         foo *= foo;
-        sin(foo);
-        //std::cout << foo.get_derivative({0,1,0,0,0,0,0}) << std::endl;
     }
-    {
-        boost::timer::auto_cpu_timer t;
-        for (auto i = 0u; i < N; ++i) {
-            audi::gdual_d x{xcoeff[i], "x",order}, y{ycoeff[i], "y",order}, z{zcoeff[i], "z",order}, w{zcoeff[i], "w",order}, q{zcoeff[i], "q",order}, r{zcoeff[i], "r",order}, s{zcoeff[i], "s",order} ;
-            auto foo = coeff[i] + x + y + z - w + q + r + s;
-            for (int j = 1; j < 25; ++j) {
-                foo *= coeff[i] + x + y + z - w + q + r + s;
-            }
-            foo *= foo;
-            sin(foo);
-            //if (i < 5) {
-            //    std::cout << foo.get_derivative({0,1,0,0,0,0,0}) << ", ";
-            //}
+    auto wall2 = t2.elapsed().wall;
+    std::cout << "Number of points in vector: " << std::setw(10) << points << "\t\tOrder: " << std::setw(3) << order << "\tSpeedup: " << std::setw(4) << wall2 / wall1 << "\n";
+}
+
+int main() {
+    for (auto j = 1u; j < 6; ++j) {
+        for (auto i = 1u; i < 4; ++i) {
+            measure_speedup(pow(10,i), j);
         }
     }
-
 }
