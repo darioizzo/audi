3bb090394c8dc0fcff0460af7e4984f84c502d11
Dario Izzo <dario.izzo@gmail.com>
2016-10-01 01:05:42 +0200

    templated gdual exposition

--- pyaudi/core.cpp
+++ pyaudi/core.cpp
@@ -28,84 +28,91 @@
 using namespace audi;
 namespace py = pybind11;
 
+template<typename T>
+void expose_gdual(const py::module &m, std::string type)
+{
+    py::class_<gdual<T>>(m,("gdual_"+type).c_str())
+    .def(py::init<>())
+    .def(py::init<const gdual<T> &>())
+    .def(py::init<double>())
+    .def(py::init<double, const std::string &, unsigned int>())
+    .def("__repr__",[](const gdual<T> &g) -> std::string {
+        std::ostringstream oss;
+        oss << g;
+        return oss.str();
+    })
+    .def("_repr_latex_",[](const gdual<T> &g) -> std::string {
+        std::ostringstream oss;
+        g._poly().print_tex(oss);
+        auto retval = oss.str();
+        retval += std::string("+\\mathcal{O}\\left(")
+            + boost::lexical_cast<std::string>(g.get_order() + 1) +  "\\right) \\]";
+        return std::string("\\[ ") + retval;
+    })
+    .def("__getstate__", [](const gdual<T> &p) {
+        // Returns a tuple that contains the string
+        // representation of a gdual<T> as obtained
+        // from the boost serialization library
+        std::stringstream ss;
+        boost::archive::text_oarchive oa(ss);
+        oa << p;
+        return py::make_tuple(ss.str());
+    })
+    .def("__setstate__", [](gdual<T> &p, py::tuple t) {
+        if (t.size() != 1)
+            throw std::runtime_error("Invalid state!");
+        // Invoke the default constructor.
+        new (&p) gdual<T>;
+        // Reconstruct the gdual<T>
+        std::stringstream ss(t[0].cast<std::string>());
+        boost::archive::text_iarchive ia(ss);
+        ia >> p;
+    })
+    .def_property_readonly("symbol_set",&gdual<T>::get_symbol_set)
+    .def_property_readonly("symbol_set_size",&gdual<T>::get_symbol_set_size)
+    .def_property_readonly("degree",&gdual<T>::degree)
+    .def_property_readonly("order",&gdual<T>::get_order)
+    .def_property_readonly("constant_cf",&gdual<T>::constant_cf)
+    .def("extend_symbol_set", &gdual<T>::extend_symbol_set, "Extends the symbol set")
+    .def("integrate", &gdual<T>::template integrate<>, "Integrate with respect to argument")
+    .def("partial", &gdual<T>::template partial<>, "Partial derivative with respect to argument")
+    .def("evaluate",[](const gdual<T> &g, const std::map< std::string, double> &dict) {return g.evaluate(std::unordered_map< std::string, double>(dict.begin(), dict.end()));} , "Evaluates the Taylor polynomial")
+    .def("find_cf", [](const gdual<T> &g, const std::vector<int> &v) {
+        return g.find_cf(v);
+    },"Find the coefficient of the Taylor expansion")
+    .def("get_derivative", [](const gdual<T> &g, const std::vector<int> &v) {
+        return g.get_derivative(v);
+    },"Finds the derivative (i.e. the coefficient of the Taylor expansion discounted of a factorial factor")
+    .def("get_derivative", [](const gdual<T> &g, const std::unordered_map<std::string, unsigned int> &dict) {
+        return g.get_derivative(dict);
+    },"Finds the derivative (i.e. the coefficient of the Taylor expansion discounted of a factorial factor")
+    .def(py::self + py::self)
+    .def(py::self - py::self)
+    .def(py::self * py::self)
+    .def(py::self / py::self)
+    .def(py::self + double())
+    .def(py::self - double())
+    .def(py::self * double())
+    .def(py::self / double())
+    .def(-py::self)
+    .def(+py::self)
+    .def(double() + py::self)
+    .def(double() - py::self)
+    .def(double() * py::self)
+    .def(double() / py::self)
+    .def(py::self == py::self)
+    .def(py::self != py::self)
+    .def("__pow__",[](const gdual<double> &g, double x) {return pow(g,x);} ,("Exponentiation (gdual_"+type+", double).").c_str())
+    .def("__pow__",[](const gdual<double> &base, const gdual<double> &g) {return pow(base,g);} ,("Exponentiation (gdual_"+type+", gdual_"+type+").").c_str())
+    .def("__rpow__",[](const gdual<double> &g, double x) {return pow(x,g);} ,("Exponentiation (double, gdual_"+type+").").c_str())
+    ;
+}
+
 PYBIND11_PLUGIN(_core) {
     py::module m("_core", "pyaudi's core module");
 
-    py::class_<gdual_d>(m,"gdual")
-        .def(py::init<>())
-        .def(py::init<const gdual_d &>())
-        .def(py::init<double>())
-        .def(py::init<double, const std::string &, unsigned int>())
-        .def("__repr__",[](const gdual_d &g) -> std::string {
-            std::ostringstream oss;
-            oss << g;
-            return oss.str();
-        })
-        .def("_repr_latex_",[](const gdual_d &g) -> std::string {
-            std::ostringstream oss;
-            g._poly().print_tex(oss);
-            auto retval = oss.str();
-            retval += std::string("+\\mathcal{O}\\left(")
-                + boost::lexical_cast<std::string>(g.get_order() + 1) +  "\\right) \\]";
-            return std::string("\\[ ") + retval;
-        })
-        .def("__getstate__", [](const gdual_d &p) {
-            // Returns a tuple that contains the string
-            // representation of a gdual_d as obtained
-            // from the boost serialization library
-            std::stringstream ss;
-            boost::archive::text_oarchive oa(ss);
-            oa << p;
-            return py::make_tuple(ss.str());
-        })
-        .def("__setstate__", [](gdual_d &p, py::tuple t) {
-            if (t.size() != 1)
-                throw std::runtime_error("Invalid state!");
-            // Invoke the default constructor.
-            new (&p) gdual_d;
-            // Reconstruct the gdual_d
-            std::stringstream ss(t[0].cast<std::string>());
-            boost::archive::text_iarchive ia(ss);
-            ia >> p;
-        })
-        .def_property_readonly("symbol_set",&gdual_d::get_symbol_set)
-        .def_property_readonly("symbol_set_size",&gdual_d::get_symbol_set_size)
-        .def_property_readonly("degree",&gdual_d::degree)
-        .def_property_readonly("order",&gdual_d::get_order)
-        .def_property_readonly("constant_cf",&gdual_d::constant_cf)
-        .def("extend_symbol_set", &gdual_d::extend_symbol_set, "Extends the symbol set")
-        .def("integrate", &gdual_d::integrate<>, "Integrate with respect to argument")
-        .def("partial", &gdual_d::partial<>, "Partial derivative with respect to argument")
-        .def("evaluate",[](const gdual_d &g, const std::map< std::string, double> &dict) {return g.evaluate(std::unordered_map< std::string, double>(dict.begin(), dict.end()));} , "Evaluates the Taylor polynomial")
-        .def("find_cf", [](const gdual_d &g, const std::vector<int> &v) {
-            return g.find_cf(v);
-        },"Find the coefficient of the Taylor expansion")
-        .def("get_derivative", [](const gdual_d &g, const std::vector<int> &v) {
-            return g.get_derivative(v);
-        },"Finds the derivative (i.e. the coefficient of the Taylor expansion discounted of a factorial factor")
-        .def("get_derivative", [](const gdual_d &g, const std::unordered_map<std::string, unsigned int> &dict) {
-            return g.get_derivative(dict);
-        },"Finds the derivative (i.e. the coefficient of the Taylor expansion discounted of a factorial factor")
-        .def(py::self + py::self)
-        .def(py::self - py::self)
-        .def(py::self * py::self)
-        .def(py::self / py::self)
-        .def(py::self + double())
-        .def(py::self - double())
-        .def(py::self * double())
-        .def(py::self / double())
-        .def(-py::self)
-        .def(+py::self)
-        .def(double() + py::self)
-        .def(double() - py::self)
-        .def(double() * py::self)
-        .def(double() / py::self)
-        .def(py::self == py::self)
-        .def(py::self != py::self)
-        .def("__pow__",[](const gdual_d &g, double x) {return pow(g,x);} ,"Exponentiation (gdual_d, double).")
-        .def("__pow__",[](const gdual_d &base, const gdual_d &g) {return pow(base,g);} ,"Exponentiation (gdual_d, gdual_d).")
-        .def("__rpow__",[](const gdual_d &g, double x) {return pow(x,g);} ,"Exponentiation (double, gdual_d).")
-    ;
+    expose_gdual<double>(m, "double");
+    expose_gdual<vectorized_double>(m, "vdouble");
 
     m.def("exp",[](const gdual_d &d) {return exp(d);},"Exponential (gdual_d).");
     m.def("exp",[](double x) {return std::exp(x);},"Exponential (double).");
@@ -165,82 +172,6 @@ PYBIND11_PLUGIN(_core) {
 
     m.def("erf",[](const gdual_d &d) {return erf(d);},"Error function (gdual_d).");
     m.def("erf",[](double x) {return std::erf(x);},"Error function (double).");
-
-    py::class_<gdual_v>(m,"gdual_v")
-        .def(py::init<>())
-        .def(py::init<const gdual_v &>())
-        .def(py::init<std::vector<double>>())
-        .def(py::init<std::vector<double>, const std::string &, unsigned int>())
-        .def("__repr__",[](const gdual_v &g) -> std::string {
-            std::ostringstream oss;
-            oss << g;
-            return oss.str();
-        })
-        .def("_repr_latex_",[](const gdual_v &g) -> std::string {
-            std::ostringstream oss;
-            g._poly().print_tex(oss);
-            auto retval = oss.str();
-            retval += std::string("+\\mathcal{O}\\left(")
-                + boost::lexical_cast<std::string>(g.get_order() + 1) +  "\\right) \\]";
-            return std::string("\\[ ") + retval;
-        })
-        .def("__getstate__", [](const gdual_v &p) {
-            // Returns a tuple that contains the string
-            // representation of a gdual_d as obtained
-            // from the boost serialization library
-            std::stringstream ss;
-            boost::archive::text_oarchive oa(ss);
-            oa << p;
-            return py::make_tuple(ss.str());
-        })
-        .def("__setstate__", [](gdual_v &p, py::tuple t) {
-            if (t.size() != 1)
-                throw std::runtime_error("Invalid state!");
-            // Invoke the default constructor.
-            new (&p) gdual_v;
-            // Reconstruct the gdual_d
-            std::stringstream ss(t[0].cast<std::string>());
-            boost::archive::text_iarchive ia(ss);
-            ia >> p;
-        })
-        .def_property_readonly("symbol_set",&gdual_v::get_symbol_set)
-        .def_property_readonly("symbol_set_size",&gdual_v::get_symbol_set_size)
-        .def_property_readonly("degree",&gdual_v::degree)
-        .def_property_readonly("order",&gdual_v::get_order)
-        .def_property_readonly("constant_cf",&gdual_v::constant_cf)
-        .def("extend_symbol_set", &gdual_v::extend_symbol_set, "Extends the symbol set")
-        .def("integrate", &gdual_v::integrate<>, "Integrate with respect to argument")
-        .def("partial", &gdual_v::partial<>, "Partial derivative with respect to argument")
-        .def("evaluate",[](const gdual_v &g, const std::map< std::string, double> &dict) {return g.evaluate(std::unordered_map< std::string, double>(dict.begin(), dict.end()));} , "Evaluates the Taylor polynomial")
-        .def("find_cf", [](const gdual_v &g, const std::vector<int> &v) {
-            return g.find_cf(v);
-        },"Find the coefficient of the Taylor expansion")
-        .def("get_derivative", [](const gdual_v &g, const std::vector<int> &v) {
-            return g.get_derivative(v);
-        },"Finds the derivative (i.e. the coefficient of the Taylor expansion discounted of a factorial factor")
-        .def("get_derivative", [](const gdual_v &g, const std::unordered_map<std::string, unsigned int> &dict) {
-            return g.get_derivative(dict);
-        },"Finds the derivative (i.e. the coefficient of the Taylor expansion discounted of a factorial factor")
-        .def(py::self + py::self)
-        .def(py::self - py::self)
-        .def(py::self * py::self)
-        .def(py::self / py::self)
-        .def(py::self + double())
-        .def(py::self - double())
-        .def(py::self * double())
-        .def(py::self / double())
-        .def(-py::self)
-        .def(+py::self)
-        .def(double() + py::self)
-        .def(double() - py::self)
-        .def(double() * py::self)
-        .def(double() / py::self)
-        .def(py::self == py::self)
-        .def(py::self != py::self)
-        .def("__pow__",[](const gdual_v &g, double x) {return pow(g,x);} ,"Exponentiation (gdual_d, double).")
-        .def("__pow__",[](const gdual_v &base, const gdual_v &g) {return pow(base,g);} ,"Exponentiation (gdual_d, gdual_d).")
-        .def("__rpow__",[](const gdual_v &g, double x) {return pow(x,g);} ,"Exponentiation (double, gdual_d).")
-    ;
 
     m.def("exp",[](const gdual_v &d) {return exp(d);},"Exponential (gdual_v).");
 
