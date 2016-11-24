de39c72dd0109df932d9ea2240924ea99f537309
Dario Izzo <dario.izzo@gmail.com>
2016-11-24 15:25:19 +0100

    One more step #6

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -69,6 +69,7 @@ print("Installing piranha")
 run_command(r'cmake -G "MinGW Makefiles" .. -DCMAKE_INSTALL_PREFIX=c:\\local ', verbose=False)
 run_command(r'mingw32-make install VERBOSE=1', verbose=False)
 os.chdir('../../')
+print("Piranha sucesfully installed .. continuing")
 
 # Set the path so that the precompiled libs can be found.
 os.environ['PATH'] = os.environ['PATH'] + r';c:\\local\\lib'
@@ -132,7 +133,7 @@ else:
 
 # Build+install step.
 #run_command(r'cmake --build . --target install')
-run_command(r'mingw32-make VERBOSE=1')
+run_command(r'mingw32-make install VERBOSE=1')
 
 # Move the shared library in the correct python DESTINATION
 #run_command(r'move "C:\Program Files (x86)\Keplerian_Toolbox\lib\libkeplerian_toolbox.dll" "c:\local\lib"')
