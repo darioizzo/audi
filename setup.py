ca47d3bc95d696e2158597e199264c5992b939f6
Dario Izzo <dario.izzo@gmail.com>
2016-11-28 17:19:12 +0100

    CI travis #1

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup, Extension
+
+extension_module = Extension(
+    'pyaudi/_core',
+     sources=['pyaudi/core.cpp'],
+     libraries=['boost_python3', 'boost_serialization', 'gmp', 'mpfr']
+)
+
+setup(
+    name = 'pyaudi',
+    version = '1.0',
+    description = 'This is a demo package with a compiled C extension.',
+    ext_modules = [extension_module],
+    packages=['pyaudi']
+)
