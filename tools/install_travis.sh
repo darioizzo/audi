47e78d47f9e865c925ed16280b5ee5ca7ba9d099
darioizzo <dario.izzo@gmail.com>
2016-11-29 12:01:30 +0100

    CI travis #27

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -8,7 +8,7 @@ set -x
 export PATH="$deps_dir/bin:$PATH"
 
 if [[ "${BUILD_TYPE}" == "Debug" ]]; then
-    cmake -DBUILD_MAIN=no -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=yes ../; 
+    cmake -DBUILD_MAIN=no -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=yes ../;
     make VERBOSE=1;
     ctest -VV
 elif [[ "${BUILD_TYPE}" == "Release" ]]; then
@@ -18,9 +18,9 @@ elif [[ "${BUILD_TYPE}" == "Release" ]]; then
 fi
 
 if [[ "${BUILD_TYPE}" == "Python2" || "${BUILD_TYPE}" == "Python3" ]]; then
-    cmake -DPIRANHA_WITH_MSGPACK=yes -DPIRANHA_WITH_BZIP2=yes -DPIRANHA_WITH_ZLIB=yes -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DBUILD_PYRANHA=yes -DCMAKE_CXX_FLAGS_DEBUG=-g0 -DCMAKE_CXX_FLAGS=-Os -DCMAKE_INSTALL_PREFIX=$deps_dir  ../;
+    cmake -DBUILD_MAIN=no -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=no -DBUILD_PYAUDI=yes ../;
     make install VERBOSE=1;
-    python -c "import pyranha.test; pyranha.test.run_test_suite()";
+    python -c "import pyaudi.test; pyaudi.test.run_test_suite()";
 fi
 
 set +e
