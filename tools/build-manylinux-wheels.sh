44152ffef3254dd409207bfc79f0ee98f4dedf5d
darioizzo <dario.izzo@gmail.com>
2016-11-29 15:03:13 +0100

    CI travis #41

--- tools/build-manylinux-wheels.sh
+++ tools/build-manylinux-wheels.sh
@@ -10,10 +10,13 @@ yum install -y mpfr
 #
 # Install piranha
 
+# Install and compile pyaudi
+wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.gz
+tar --bzip2 -xf /io/boost_1_62_0.tar.bz2
+
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    ${PYBIN}/pip install -r numpy 
     ${PYBIN}/pip wheel /io/ -w wheelhouse/
 done
 
