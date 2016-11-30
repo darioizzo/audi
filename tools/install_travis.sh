56912cc85078b8d6eec65d7c7bf0958b25d2bc71
darioizzo <dario.izzo@gmail.com>
2016-11-30 10:43:36 +0100

    new travis CI #5

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -8,12 +8,14 @@ cd /io
 mkdir lib
 echo "env:"
 echo ${BUILD_TYPE}
+echo ${PATH_TO_PYTHON}
+echo ${PYTHON_VERSION}
 # Install boost
 wget --no-check-certificate https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.bz2
-tar --bzip2 -xf /io/boost_1_62_0.tar.bz2
+tar --bzip2 -xf /io/boost_1_62_0.tar.bz2 > /dev/null 2>&1
 cd boost_1_62_0
 ./bootstrap.sh
-echo "using python : 3.5 : /opt/python/cp35-cp35m/bin/python3 : /opt/python/cp35-cp35m/include/python3.5m : /opt/python/cp35-cp35m/lib;" >> project-config.jam
+echo "using python : ${PYTHON_VERSION} : ${PATH_TO_PYTHON}/bin/python : ${PATH_TO_PYTHON}/include/python3.5m : ${PATH_TO_PYTHON}/lib;" >> project-config.jam
 ./b2 install --with-python --with-serialization
 
 #
