d91b7dbcccde30665be261075399eaa2e68d744c
darioizzo <dario.izzo@gmail.com>
2016-11-30 09:15:20 +0100

    Last before complete change on travis

--- tools/build-manylinux-wheels.sh
+++ tools/build-manylinux-wheels.sh
@@ -5,12 +5,14 @@ yum install -y gmp
 yum install -y mpfr
 
 cd /io
-mkdir libs
+mkdir lib
 # Install boost
-wget https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.bz2 --no-check-certificate
+wget --no-check-certificate https://sourceforge.net/projects/boost/files/boost/1.62.0/boost_1_62_0.tar.bz2
 tar --bzip2 -xf /io/boost_1_62_0.tar.bz2
 cd boost_1_62_0
-./bootstrap.sh --prefix=/io/libs
+./bootstrap.sh
+echo "using python : 3.5 : /opt/python/cp35-cp35m/bin/python3 : /opt/python/cp35-cp35m/include/python3.5m : /opt/python/cp35-cp35m/lib;" >> project-config.jam
+./b2 install --with-python --with-serialization
 
 #
 # Install cmake
@@ -36,3 +38,6 @@ for PYBIN in /opt/python/*/bin/; do
     ${PYBIN}/pip install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
 done
+
+# Python configuration
+using python : 3.5 : /opt/python/cp35-cp35m/bin/python3 : /opt/python/cp35-cp35m/include/python3.5m : /opt/python/cp35-cp35m/lib;
