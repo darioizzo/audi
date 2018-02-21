7c89491bd496e254563331af2bbf2204ffd506ca
darioizzo <dario.izzo@gmail.com>
2018-02-21 10:30:29 +0100

    New CI #8

--- tools/install_docker.sh
+++ tools/install_docker.sh
@@ -41,8 +41,7 @@ mkdir install
 cd install
 
 # Install Boost https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2
-wget https://dl.bintray.com/boostorg/release/${BOOST_VERSION}/source/boost_`echo ${BOOST_VERSION}|tr "." "_"`.tar.bz2 --no-verbose --no-check-certificate
-ls
+wget https://downloads.sourceforge.net/project/boost/boost/${BOOST_VERSION}/boost_`echo ${BOOST_VERSION}|tr "." "_"`.tar.bz2 --no-verbose
 tar xvf boost_`echo ${BOOST_VERSION}|tr "." "_"`.tar.bz2
 cd boost_`echo ${BOOST_VERSION}|tr "." "_"`
 sh bootstrap.sh --with-python=/opt/python/${PYTHON_DIR}/bin/python > /dev/null
