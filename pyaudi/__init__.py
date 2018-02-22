ee1b5dcc9a4173ed8e5af7a8b87ed2955836e85a
darioizzo <dario.izzo@gmail.com>
2018-02-22 10:37:30 +0100

    [Appveyor] - trying with python #5

--- pyaudi/__init__.py
+++ pyaudi/__init__.py
@@ -1,4 +1,8 @@
+# for python 2.0 compatibility
+from __future__ import absolute_import as _ai
+
+# Version setup.
+from ._version import __version__
+
 __all__ = ['test']
 from .core import *
-
-__version__ = {'major': 1, 'minor': 0, 'bugfix': 3}
