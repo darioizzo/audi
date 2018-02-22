516714d85ef9b497af2af9bd46aacf87215afdbb
Dario Izzo <dario.izzo@gmail.com>
2018-02-22 20:47:11 +0100

    [appveyor] CI round

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -172,10 +172,8 @@ if is_python_build:
     run_command(pinterp + r' setup.py bdist_wheel')
     os.environ['PATH'] = ORIGINAL_PATH
     run_command(pinterp + r' -m pip install dist\\' + os.listdir('dist')[0])
-
-    os.chdir('/')
     run_command(
-        pinterp + r' -c "from pyaudi import test; test.run_test_suite()"')
+        pinterp + r' -c "from pyaudi import test; test.run_test_suite()"', directory=r'c:\\')
     if is_release_build:
         os.chdir('C:/projects/audi/build/wheel')
         run_command(twine + r' upload -u darioizzo dist\\' +
