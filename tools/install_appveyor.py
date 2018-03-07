4ff4bd4b8e80cdffee5ec2d24325754df33c5365
Dario Izzo <dario.izzo@gmail.com>
2018-03-07 19:51:48 +0100

    [CI Debug] pip workaround

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -122,8 +122,10 @@ if is_python_build:
     # Install pip and deps.
     wget(r'https://bootstrap.pypa.io/get-pip.py', 'get-pip.py')
     run_command(pinterp + ' get-pip.py')
-    # if is_release_build:
-    run_command(pip + ' install twine')
+    if is_release_build:
+        # call pip via python, workaround to avoid path issues when calling pip from win
+        # (https://github.com/pypa/pip/issues/1997)
+        run_command(pinterp + r' -m pip install twine')
 
 # Set the path so that the precompiled libs can be found.
 os.environ['PATH'] = os.environ['PATH'] + r';c:\\local\\lib'
