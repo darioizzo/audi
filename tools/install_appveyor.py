7e57fc6172047e9696603179449fc47530674bbc
Dario Izzo <dario.izzo@gmail.com>
2018-05-25 11:39:06 +0200

    activating mppp #3

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -166,7 +166,7 @@ elif BUILD_TYPE in ['Release', 'Debug']:
     cmake_opts = r'-DCMAKE_BUILD_TYPE=' + BUILD_TYPE + \
         r' -DAUDI_BUILD_TESTS=yes ' + common_cmake_opts
     run_command(r'cmake -G "MinGW Makefiles" .. ' + cmake_opts)
-    run_command(r'mingw32-make install VERBOSE=1 -Wa -mbig-obj')
+    run_command(r'mingw32-make install VERBOSE=1')
     run_command(r'ctest')
 else:
     raise RuntimeError('Unsupported build type: ' + BUILD_TYPE)
