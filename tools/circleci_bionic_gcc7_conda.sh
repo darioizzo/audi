4115b9e8fcf1a1af0b86aa17c109ea4815a5375d
Francesco Biscani <bluescarni@gmail.com>
2019-10-21 12:06:06 +0200

    First commit of circleci files.

---
+++ tools/circleci_bionic_gcc7_conda.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install build-essential
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda
+conda config --add channels conda-forge --force
+
+conda_pkgs="cmake>=3.3 eigen obake mppp boost-cpp"
+
+conda create -q -p $deps_dir -y $conda_pkgs
+source activate $deps_dir
+
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+export PATH="$deps_dir/bin:$PATH"
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Release -DAUDI_BUILD_AUDI=yes -DAUDI_BUILD_TESTS=yes
+make -j2 VERBOSE=1
+ctest -j4 -V
+
+set +e
+set +x
