7430ed8217a18dbc838ea05e49b9cba49e6266da
Francesco Biscani <bluescarni@gmail.com>
2019-10-21 13:05:12 +0200

    First attempt at pyaudi build.

--- tools/circleci_bionic_gcc7_conda.sh
+++ tools/circleci_bionic_gcc7_conda.sh
@@ -15,7 +15,7 @@ export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
 conda config --add channels conda-forge --force
 
-conda_pkgs="cmake>=3.3 eigen obake-devel mppp boost-cpp"
+conda_pkgs="cmake>=3.3 eigen obake-devel mppp boost boost-cpp python=3.7"
 
 conda create -q -p $deps_dir -y $conda_pkgs
 source activate $deps_dir
@@ -29,8 +29,15 @@ mkdir build
 cd build
 
 cmake ../ -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DAUDI_BUILD_AUDI=yes -DAUDI_BUILD_TESTS=yes
-make -j2 VERBOSE=1
+make -j2 VERBOSE=1 install
 ctest -j4 -V
+
+cd ..
+mkdir build_python
+cd build_python
+cmake ../ -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DAUDI_BUILD_AUDI=no -DAUDI_BUILD_PYAUDI=yes
+make -j2 VERBOSE=1 install
+python -c "import pyaudi.test; pyaudi.test.run_test_suite()"
 
 set +e
 set +x
